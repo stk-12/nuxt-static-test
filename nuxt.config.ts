6b8422a74730eb830505d8c30fd57feb59f689eb
keisuke sato <corydoras121@gmail.com>
2024-08-13 17:41:39 +0900

    ルーティング設定カスタム /index.htmlでもアクセス可能に

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,21 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  ssr: true,
+  ssr: true, // SSRを有効にして、SSGモードを使用
   // ssr: false, // falseにするとアプリケーションがSPAとして動作するようになる
   app: {
-    baseURL: '/nuxt_demo_mpa/'
+    baseURL: '/nuxt_demo_mpa/',
+  },
+  hooks: {
+    // カスタムルート設定
+    'pages:extend' (routes) {
+      // for (const route of routes) {
+      //   route.alias = resolve(route.path, 'index.html')
+      // }
+      routes.forEach((route) => {
+        const alias = route.path.length > 1 ? `${route.path}/index.html` : '/index.html'        
+        route.alias = alias
+      })
+    }
   },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,8 @@
   <div>
     <h1>Top Page</h1>
     
-    <NuxtLink to="/about">Aboutページへ</NuxtLink>
-    <NuxtLink to="/company">Companyページへ</NuxtLink>
+    <NuxtLink to="/about/">Aboutページへ</NuxtLink>
+    <NuxtLink to="/company/">Companyページへ</NuxtLink>
+    <a href="/nuxt_demo_mpa/about/">Aboutへ（aタグ）</a>
   </div>
 </template>
